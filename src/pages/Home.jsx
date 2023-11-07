10e4567b0dc37677b77cdd10667a23c0c74b31f1
Jihad-24 <mdjihat0021@example.com>
2023-11-07 20:04:25 +0600

    added find us section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import Banner from "../components/Banner";
 import CategoryTabs from "../components/CategoryTabs";
+import FindUs from "../components/FindUs";
 import StartToday from "../components/StartToday";
 
 const Home = () => {
@@ -16,6 +17,15 @@ const Home = () => {
                     </div>
                 </div>
                 <StartToday></StartToday>
+            </div>
+            <div className="mb-16">
+                <div className="text-center">
+                    <h1 className="font-extrabold text-5xl text-center pb-14">Find us on the <span className="text-[#263077] italic" data-aos="fade-down">Business Listing Websites</span>
+                        <br />
+                        <span className="border-b-8 border-[#263077] pt-6  w-20 h-2 absolute" ></span></h1>
+                    <p className="font-normal text-[#3F3F3F] text-center text-xl" >We are committed to making it as convenient as possible for you to find and engage with our platform. Our presence on these business listing websites will help you connect with us in a variety of ways. Here are some of the platforms where you can find us</p>
+                </div>
+                <FindUs></FindUs>
             </div>
         </div>
     );
